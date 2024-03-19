9ba3022f79b30ecda9a0b3e267e64c2297dba481
Hannah Bradley <hannahthesailor@yahoo.com>
2024-03-19 02:35:27 -0400

    python-p3-inheritance-lab.md

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,12 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else [""]
 
     def teach(self):
-        pass
+        if self.knowledge:
+            return random.choice(self.knowledge)
+        else:
+            return None
